8d313e2e91616012ecd2a2c2101ba034c93e8716
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-24 23:36:58 +0500

    Task1

---
+++ Tyuiu.ChirchenkoME.Sprint3.Task1.V23.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ChirchenkoME.Sprint3.Task1.V23.Lib
+{
+    public class DataService : ISprint3Task1V23
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double x = value;
+            double product = 1.0;
+            int k = startValue;
+
+            while (k <= stopValue)
+            {
+                double denominator = Math.Sin(x) + Math.Pow(x, k);
+                double term = Math.Pow(300.0 / denominator, k);
+                product *= term;
+
+                k++;
+            }
+
+            return Math.Round(product, 3);
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint3.Task1.V23/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ChirchenkoME.Sprint3.Task1.V23.Lib;
+namespace Tyuiu.ChirchenkoME.Sprint3.Task1.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Чирченко М. Е. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Чирченко М. Е. | ИСПб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, используя цикл while, которая вычисляет             *");
+            Console.WriteLine("* произведение ряда по формуле:                                           *");
+            Console.WriteLine("*       5   (   300   )^k                                                 *");
+            Console.WriteLine("* p =   Π   ( ------- )                                                   *");
+            Console.WriteLine("*      k=1   ( sin(x)+x^k )                                               *");
+            Console.WriteLine("* при x = 5                                                               *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
+
+            Console.WriteLine("x = " + value);
+            Console.WriteLine("Start = " + startValue);
+            Console.WriteLine("Stop = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.GetMultiplySeries(value, startValue, stopValue);
+            Console.WriteLine("Произведение ряда p = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
