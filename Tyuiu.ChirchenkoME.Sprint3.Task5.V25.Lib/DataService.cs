ea19f9614857c7ae8a069652fcc4acae1de07654
BillyHarrington1 <datskiydenis@gmail.com>
2025-10-25 00:03:29 +0500

    Task5

---
+++ Tyuiu.ChirchenkoME.Sprint3.Task5.V25.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ChirchenkoME.Sprint3.Task5.V25.Lib
+{
+    public class DataService : ISprint3Task5V25
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sum = 0.0;
+
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    double term = Math.Pow(x, k) + Math.Cos(k);
+                    sum += term;
+                }
+            }
+
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint3.Task5.V25/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ChirchenkoME.Sprint3.Task5.V25.Lib;
+namespace Tyuiu.ChirchenkoME.Sprint3.Task5.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Чирченко М. Е. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Чирченко М. Е. | ИСПб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение функции:                                             *");
+            Console.WriteLine("* y = Z3 Z10 (x^k + cos(k)), при x=2                                      *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("i от " + startValue1 + " до " + stopValue1);
+            Console.WriteLine("k от " + startValue2 + " до " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            Console.WriteLine("Значение y = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
